b4755016b280e70037d2fb6d12ba8791c853ef83
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-06-22 12:08:35 -0300

    email add

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -3,11 +3,18 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { EmailService } from '../services/Email/EmailService';
 @Controller('email')
 export class EmailController {
-    constructor(private readonly emailService: EmailService) {}
+  constructor(private readonly emailService: EmailService) {}
 
-    @Post('enviar')
-    async enviarEmail(@Body() body: { destinatario: string, assunto: string, corpo: string }): Promise<void> {
-      const { destinatario, assunto, corpo } = body;
+  @Post('enviar')
+  async enviarEmail(
+    @Body() body: { destinatario: string; assunto: string; corpo: string },
+  ): Promise<void> {
+    const { destinatario, assunto, corpo } = body;
+    try {
       await this.emailService.enviarEmail(destinatario, assunto, corpo);
+    } catch (error) {
+      console.log(error);
+      return error;
     }
+  }
 }
